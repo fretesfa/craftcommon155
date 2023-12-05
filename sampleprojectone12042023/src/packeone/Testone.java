aa90c10ec25283ab67972d81bfc391ae0af89f02
fre <fregetnet09@gmail.com>
2023-12-04 21:10:31 -0500

    my eclipse commit

--- sampleprojectone12042023/src/packeone/Testone.java
+++ sampleprojectone12042023/src/packeone/Testone.java
@@ -5,7 +5,7 @@ public class Testone {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("greeting");
-System.out.println("thank you");
+System.out.println("thank you.");
 System.out.println("problem.");
 	}
 
