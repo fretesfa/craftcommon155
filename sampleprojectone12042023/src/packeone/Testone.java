0380254e8c1377149529bb582ec043e8cf164a92
fre <fregetnet09@gmail.com>
2023-12-04 20:36:55 -0500

    commit from eclipse

---
+++ sampleprojectone12042023/src/packeone/Testone.java
@@ -0,0 +1,11 @@
+package packeone;
+
+public class Testone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("greeting");
+System.out.println("thank you");
+	}
+
+}
